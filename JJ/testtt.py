39cb1f6c27236d59003c1456fbed9b2acf9c1931
오종진 <45455262+Oh-JongJin@users.noreply.github.com>
2020-03-11 16:41:50 +0900

    0311_now

--- JJ/testtt.py
+++ JJ/testtt.py
@@ -7,16 +7,30 @@ from PyQt5 import QtWidgets, QtGui
 from PyQt5 import QtGui, QtCore
 import sys
 import socket
+import pymysql
+import time
 
 HOST = '192.168.0.144'
-PORT = 8888
+PORT = 8080
+
+
+class manager_menu(QMainWindow):
+    def __init__(self, parent=None):
+        super(manager_menu, self).__init__(parent)
 
 
 class sudo_menu(QMainWindow):
     def __init__(self, parent=None):
         super(sudo_menu, self).__init__(parent)
-        self.setGeometry(350, 350, 600, 500)
-        self.setWindowTitle('Second Page')
+
+        self.mainlabel_txt = QLabel(self)
+        self.mainlabel_txt.setText("슈퍼 유저 메뉴")
+        # self.mainlabel_txt.setFixedSize(1180, 512)
+
+        self.mainlabel_txt.move(130, 80)
+
+        self.setGeometry(150, 300, 610, 500)
+        self.setWindowTitle('슈퍼 관리자 메뉴')
         self.show()
 
 
@@ -28,79 +42,108 @@ class First(QMainWindow):
         self.input_text = QLineEdit(self)
         self.input_text.resize(300, 30)
 
-        send_btn = QPushButton(self)
-        send_btn.setText('보내기')
-        send_btn.resize(70, 30)
+        self.send_btn = QPushButton(self)
+        self.send_btn.setText('보내기')
+        self.send_btn.resize(70, 30)
 
-        sudo_btn = QPushButton(self)
-        sudo_btn.setText('슈퍼 관리자 메뉴')
-        sudo_btn.resize(160, 150)
+        self.sudo_btn = QPushButton(self)
+        self.sudo_btn.setText('슈퍼 관리자 메뉴')
+        self.sudo_btn.resize(160, 150)
 
-        manager_btn = QPushButton(self)
-        manager_btn.setText('관리자 메뉴')
-        manager_btn.resize(160, 150)
+        self.manager_btn = QPushButton(self)
+        self.manager_btn.setText('관리자 메뉴')
+        self.manager_btn.resize(160, 150)
 
-        staff_btn = QPushButton(self)
-        staff_btn.setText('직원 메뉴')
-        staff_btn.resize(160, 150)
+        self.staff_btn = QPushButton(self)
+        self.staff_btn.setText('직원 메뉴')
+        self.staff_btn.resize(160, 150)
 
-        send_txt = QLabel(self)
-        send_txt.setText('송신')
+        self.send_txt = QLabel(self)
+        self.send_txt.setText('송신')
 
-        recv_txt = QLabel(self)
-        recv_txt.setText('수신')
+        self.recv_txt = QLabel(self)
+        self.recv_txt.setText('수신')
 
-        send_txtB = QTextBrowser(self)
-        send_txtB.resize(250, 150)
+        self.send_txtB = QTextBrowser(self)
+        self.send_txtB.resize(250, 150)
 
-        recv_txtB = QTextBrowser(self)
-        recv_txtB.resize(250, 150)
+        self.recv_txtB = QTextBrowser(self)
+        self.recv_txtB.resize(250, 150)
 
         self.input_text.move(25, 25)
-        send_btn.move(350, 25)
-        sudo_btn.move(25, 100)
-        manager_btn.move(220, 100)
-        staff_btn.move(415, 100)
-        send_txt.move(130, 270)
-        recv_txt.move(430, 270)
-        send_txtB.move(25, 300)
-        recv_txtB.move(325, 300)
+        self.send_btn.move(350, 25)
+        self.sudo_btn.move(25, 100)
+        self.manager_btn.move(220, 100)
+        self.staff_btn.move(415, 100)
+        self.send_txt.move(130, 270)
+        self.recv_txt.move(430, 270)
+        self.send_txtB.move(25, 300)
+        self.recv_txtB.move(325, 300)
 
-        send_btn.clicked.connect(self.send_btn_clicked)
+        self.send_btn.clicked.connect(self.send_btn_clicked)
 
-        sudo_btn.clicked.connect(self.sudo_btn_clicked)
-        sudo_btn_dialogs = list()
+        self.sudo_btn.clicked.connect(self.sudo_btn_clicked)
+        self.sudo_btn_dialogs = list()
 
-        manager_btn.clicked.connect(self.manager_btn_clicked)
-        manager_btn_dialogs = list()
+        self.manager_btn.clicked.connect(self.manager_btn_clicked)
+        self.manager_btn_dialogs = list()
+
+        self.staff_btn.clicked.connect(self.staff_btn_clicked)
+        self.staff_btn_dialogs = list()
 
         self.setGeometry(150, 300, 610, 500)
-        self.setWindowTitle('Testtt.py')
+        self.setWindowTitle('스마트 팩토리')
         self.show()
 
+
     def send_btn_clicked(self):
+        now = time.localtime()
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((HOST, PORT))
-        msg = self.input_text()
+        msg = self.input_text.text()
         s.send(msg.encode(encoding='utf_8', errors='strict'))
 
         data = s.recv(1024)
-        # self.send_txtB.setText("' " + self.send_txt.text() + " '")
+        self.send_txtB.append("%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+ + "\n송신 : " + msg + "\n")
         # self.send_txtB.adjustSize()
-        # self.recv_txtB.setText("' " + data + " '")
-        print('수신 데이터 : ' + data.decode('utf_8'))
+        self.recv_txtB.append("%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+ + "\n수신 : " + str(data) + "\n")
+        # print('수신 데이터 : ' + data.decode('utf_8'))
+        s.close()
+
 
     def sudo_btn_clicked(self):
-        self.close()
-        sudo_btn_dialogs = sudo_menu(self)
-        self.sudo_btn_dialogs.append(sudo_btn_dialogs)
-        sudo_btn_dialogs.show()
+        # self.close()
+        # sudo_btn_dialogs = sudo_menu(self)
+        # self.sudo_btn_dialogs.append(sudo_btn_dialogs)
+        # sudo_btn_dialogs.show()
+
+        #그냥 해본거
+        msg = self.input_text.text()
+        self.send_txtB.setText('송신 : ' + msg)
+
+        con = pymysql.connect(host="192.168.0.2", user="root", password="1541", db='db1', charset='utf8')
+        cur = con.cursor()
+        # curs = con.cursor()
+        sql = "select distinct name from goods;"
+        cur.execute(sql)
+        # con.commit()
+        data = cur.fetchone()
+        # data1 = curs.fetchone()
+        # self.recv_txtB.setText(str(cur.execute(sql)))
+        self.recv_txtB.setText('이름 : ' + str(data[0]))
+
 
     def manager_btn_clicked(self):
         self.close()
-        sudo_btn_dialogs = sudo_menu(self)
-        self.sudo_btn_dialogs.append(sudo_btn_dialogs)
-        sudo_btn_dialogs.show()
+        manager_btn_dialogs = manager_menu(self)
+        self.sudo_btn_dialogs.append(manager_btn_dialogs)
+        manager_btn_dialogs.show()
+
+
+    def staff_btn_clicked(self):
+        self.close()
 
 
 def main():
