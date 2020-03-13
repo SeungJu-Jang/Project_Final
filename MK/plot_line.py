381b6f88cb54b9cb152556677379c71d25a2fec5
hanminky <61956445+hanminky@users.noreply.github.com>
2020-03-13 17:51:12 +0900

    PC 앱 막대(price) 선(line) 그래프.py

--- MK/plot_line.py
+++ MK/plot_line.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import self as self
+from PyQt5.QtWidgets import QHBoxLayout, QDialog, QApplication
+from matplotlib.backends.backend_template import FigureCanvas
+from numpy import nan as NA
+import pandas as pd
 import numpy as np
+from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
-
-import PyQt5
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-
-from PyQt5.QtWidgets import QApplication, QHBoxLayout, QDialog
 import pymysql
+
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 class MainDialog(QDialog):
     def __init__(self):
         QDialog.__init__(self, None)
+
+        fig = plt.figure()
+
+        ax = fig.add_subplot(1, 1, 1)
+
+        # data = DataFrame(np.random.rand(30), columns=['col'])
+
+        ax.set_title('test')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
 
         con = pymysql.connect(host="192.168.0.34", user="root", password="1234", db='mydb', charset='utf8')
         cur = con.cursor()
@@ -23,59 +33,34 @@ class MainDialog(QDialog):
         cur.execute(sql)
         data = cur.fetchone()
         price1 = data[0]
+        ax.plot(data[0])
 
         sql = "select inventory*unitprice from pro_info WHERE idproducts = '2'";
         cur.execute(sql)
         data = cur.fetchone()
         price2 = data[0]
+        ax.plot(data[0])
 
         sql = "select inventory*unitprice from pro_info WHERE idproducts = '3'";
         cur.execute(sql)
         data = cur.fetchone()
         price3 = data[0]
+        ax.plot(data[0])
 
         sql = "select inventory*unitprice from pro_info WHERE idproducts = '4'";
         cur.execute(sql)
         data = cur.fetchone()
         price4 = data[0]
+        ax.plot(data[0])
 
-        sql = "select inventory*unitprice from pro_info WHERE idproducts = '5'";
-        cur.execute(sql)
-        data = cur.fetchone()
-        price5 = data[0]
 
-        N = 5
-        value = (price1, price2, price3, price4, price5)
-        ind = np.arange(N)
-        width = 0.35
+        ax.set_xticks([price1, price2, price3, price4])
+        ax.set_xticklabels(['one', 'two', 'three', 'four'], rotation=30, fontsize=7)
 
-        fig = plt.Figure()
-        ax = fig.add_subplot(111)
-        # ax.set_ylim([0,100])
-        ax.bar(ind, value, width)
-        ax.set_xticks(ind + width / 20)
-        ax.set_xticklabels(['price1', 'price2', 'price3', 'price4', 'price5'])
+        # ax.set_ylim([0, 1.5])
 
         canvas = FigureCanvas(fig)
         canvas.draw()
-
-
-        # N = 5
-        # plt.title("title")
-        # plt.xlabel("product")
-        # plt.ylabel("price")
-        # value = (20, 35, 30, 35, 27)
-        # ind = np.arange(N)
-        # width = 0.35
-        #
-        # plt.bar(ind, value, width)
-        #
-        # plt.ylabel('Scores')
-        # plt.title('Scores by group')
-        # plt.xticks(ind, ('G1', 'G2', 'G3', 'G4', 'G5'))
-        #
-        # plt.show()
-
 
         lay = QHBoxLayout()
         self.setLayout(lay)
