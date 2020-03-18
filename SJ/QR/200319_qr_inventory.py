be86822041695b89b520780604bd0850a00a1155
SeungJu-Jang <bewaq0317@gmail.com>
2020-03-19 02:23:51 +0900

    200319_02:25

--- SJ/QR/200319_qr_inventory.py
+++ SJ/QR/200319_qr_inventory.py
@@ -4,11 +4,11 @@ con = pymysql.connect(host="192.168.0.2", user="root", password="1q2w3e",
                        db='db_1', charset='utf8' )
 
 cur = con.cursor()
-print("재고 최신화를 할 상품명 입력")
-goods=int(input())
-num=int(input())
-print(goods)
-print(num)
+print("재고 최신화를 할 상품번호 입력")
+goods=int(input()) # 1번은 샴푸 2번은 린스 등 일경우
+print("감소할 재고의 량 입력 QR 에서는 무조건 1이겠죠?!")
+num=int(input()) #qr 코드에서는 찍어서 나온값을 num에 넣어주세요!
+
 
 sql="select inventory from test1 where goods_numb=%s"
 numb=cur.execute(sql,(goods))
@@ -17,5 +17,5 @@ for j in range(numb):
     for i in range(0,1):
         inventory=data[j]
     inventory[0]-=num
-    print(inventory)
+print(inventory[0])
     
